7ce1de05b1357baa14d2d1fcfba03227ec15c75a
MD ABIR MAHMUD <abirmahmud5665@gmail.com>
2026-08-17 10:12:22 +0600

    chore: setup prettier, list-stagged, eslint and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,60 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  // Custom Strict & Production Rules
+  {
+    rules: {
+      // General JavaScript / Clean Code
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-debugger": "error",
+      "no-alert": "error",
+      "no-var": "error",
+      "prefer-const": "error",
+      "eqeqeq": ["error", "always", { null: "ignore" }],
+      "no-duplicate-imports": "error",
+      "no-eval": "error",
+      "no-implied-eval": "error",
+      "no-new-func": "error",
+      "no-unneeded-ternary": "error",
+      "object-shorthand": ["error", "always"],
+      "prefer-template": "error",
+
+      // Strict TypeScript Rules
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports", fixStyle: "inline-type-imports" },
+      ],
+      "@typescript-eslint/no-non-null-assertion": "warn",
+
+      // Next.js & React Performance / Best Practices
+      "@next/next/no-img-element": "error",
+      "@next/next/no-html-link-for-pages": "error",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "node_modules/**",
   ]),
 ]);
 
